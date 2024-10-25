0cdf570c9e35fed7d94ade07f5e88fe05920046d
avaniavani371 <aavani371@gmail.com>
2024-10-26 02:22:45 +0530

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:HomeComponent
+
+    }
+    ,
+    {
+        path:'about',component:AboutComponent
+
+
+    }
+   
+    
+];
